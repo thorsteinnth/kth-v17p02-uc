10a55f0908417886d70d8bc29120201deb12eda3
Þorsteinn Þorri Sigurðsson <thorsteinnth@gmail.com>
2017-04-21 11:26:29 +0200

    geoman initial commit

---
+++ geoman/geoman/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  geoman
+//
+//  Created by Þorsteinn Þorri Sigurðsson on 21/04/2017.
+//  Copyright © 2017 ttsifannar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
