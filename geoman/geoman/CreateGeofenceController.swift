7430f2ccade3faba9f32bc530befcb7e06785c53
Þorsteinn Þorri Sigurðsson <thorsteinnth@gmail.com>
2017-04-27 18:04:19 +0200

    get current location

--- geoman/geoman/CreateGeofenceController.swift
+++ geoman/geoman/CreateGeofenceController.swift
@@ -7,18 +7,62 @@
 //
 
 import UIKit
+import CoreLocation
 
 class CreateGeofenceController: UIViewController {
 	
 	@IBOutlet weak var lblCreateGeofence: UILabel!
+	let locationManager = CLLocationManager()
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		lblCreateGeofence.text = "Should create geofence here";
+		// Location manager setup
+		self.locationManager.requestAlwaysAuthorization()
+		if (CLLocationManager.locationServicesEnabled()) {
+			locationManager.delegate = self
+			locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+			locationManager.startUpdatingLocation()
+		}
 	}
 	
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
 	}
+}
+
+extension CreateGeofenceController: CLLocationManagerDelegate {
+	
+	func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+		// The authorization state for the application changed
+		var locationStatus = String()
+		switch status {
+		case CLAuthorizationStatus.restricted:
+			locationStatus = "Restricted"
+		case CLAuthorizationStatus.denied:
+			locationStatus = "Denied"
+		case CLAuthorizationStatus.notDetermined:
+			locationStatus = "Not determined"
+		case CLAuthorizationStatus.authorizedAlways:
+			locationStatus = "Authorized always"
+		case CLAuthorizationStatus.authorizedWhenInUse:
+			locationStatus = "Authorized when in use"
+		default:
+			locationStatus = "Unknown location authorization status"
+		}
+		print("CLLocationManager.didChangeAuthorization - \(locationStatus)")
+	}
+	
+	func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+		// The latest location is at the end of the array
+		if let latestLocation = locations.last {
+			// The latest location is not nil
+			print("CLLocationManager.didUpdateLocations - \(latestLocation)")
+		}
+		else {
+			print("CLLocationManager.didUpdateLocations - nil")
+		}
+	}
+	
 }
